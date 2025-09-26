06bef451bd7760210e705b92268e26739337f72e
Ricardo Schneider <ricardo.schneider@de.markant.com>
2025-09-26 08:39:31 +0200

    Fix: Entferne github_token.txt - Token nur in config.py

--- config.py
+++ config.py
@@ -17,5 +17,5 @@ TEAM_CONFIG = {
 GITHUB_CONFIG = {
     "username": "ricardoschneider93",           # Dein GitHub Username
     "repo_name": "lol-team-stats",              # Repository Name
-    "token": "",                                # Token wird als Parameter übergeben
+    "token": "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",                                # Token wird als Parameter übergeben
 }
